0939ed8717863be79c4490a3369eb2aa95ad359d
Pooja <arorapooja0102@gmail.com>
2022-04-11 08:18:03 +0530

    Added Maven Project

---
+++ src/main/java/com/wayfair/wayfairassignment/WayfairAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.wayfair.wayfairassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayfairAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayfairAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wayfair/wayfairassignment/WayfairAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wayfair.wayfairassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayfairAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
